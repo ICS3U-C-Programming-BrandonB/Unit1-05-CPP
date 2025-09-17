ae999416e767608be2a28c1749fcb47551c75f18
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-09-17 18:01:21 +0000

    area and perimeter of a rectangle c++

---
+++ areaPerRect.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2025 Brandon All rights reserved.
+
+// Created By : Brandon
+// Date : September 17th, 2025
+// This program displays area and perimeter of a rectangle in c++
+
+#include <iostream>
+
+int main() {
+    std::cout << "For a rectangle that has dimensions\n";
+    std::cout << "Length 7cm and width 4cm:\n";
+    std::cout << "\n";
+    std::cout << "Area = " << 7 * 4 << " cm^2\n";
+    std::cout << "Perimeter = " << 2 * (7 + 4) << " cm\n";
+}
